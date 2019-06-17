5ad7013d9a1bbe1b82fcb2d2ca8bc0fc133a5091
MrPikac <milos.novisad@icloud.com>
2019-06-17 16:52:08 +0200

    dsjkfnsdjk

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="about">
     <h1>This is an about page that will be my wallet page</h1>
+    <div class="red">
+      <p>Tell us how much money you have in your wallet currently</p>
+    </div>
+      <br>
+      <div>
+      <input type="number" name="money" id="money">
+      <br>
+      <br>
+      <button>I'm rich bitch</button>
+      </div>
   </div>
 </template>
+
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #ff9580;
+}
+h1 {
+  background-color: aquamarine;
+}
+.red {
+  width: 100%;
+  background-color: aquamarine;
+  text-align: center;
+  color: red;
+}
+input {
+  background-color: aquamarine;
+}
+button{
+  background-color: aquamarine;
+}
+</style>
